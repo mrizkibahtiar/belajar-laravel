1ef53eb8773095547cfe7f5db7bbd3ca8ff60779
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-06-23 21:52:13 +0700

    Add initial routes, testing setup, and configuration files
    - Created console command for inspiring quotes in routes/console.php
    - Defined web routes for home, about, blog, and contact pages in routes/web.php
    - Added .gitignore files for various storage directories to exclude unnecessary files
    - Implemented basic feature and unit tests in tests/Feature/ExampleTest.php and tests/Unit/ExampleTest.php
    - Set up Pest testing framework in tests/Pest.php
    - Established base TestCase class in tests/TestCase.php
    - Configured Vite for asset management in vite.config.js

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contact</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https:instagram.com/mrzbahtiar">Instagram</a>
+        </li>
+        <li>
+            <a href="https:instagram.com/mrzbahtiar">Linkedin</a>
+        </li>
+    </ul>
+</body>
+
+</html>
