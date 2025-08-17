8fa203da4e76df95d1b6924fe8542f9f738f501f
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-08-17 14:08:31 +0700

    minor update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,53 +1,17 @@
 <?php
 
-// harus ditambahkan namespace agar bisa dipakai di tempat lain
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Support\Arr;
-
-class Post
+// secara default gabungnya dengan tabel posts(jamak)
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Muhammad Rizki Bahtiar',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt optio consectetur
-            tempore illum odio,
-            incidunt obcaecati nobis consequuntur tenetur magnam cum rem reprehenderit odit sed impedit quia molestias
-            quas? Quasi.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Muhammad Rizki Bahtiar',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum nihil magni sequi
-            delectus nisi minima asperiores sed, ullam possimus ab reprehenderit beatae eius cumque voluptatem, aperiam
-            inventore provident, tenetur doloremque!'
-            ],
-        ];
-    }
 
-    public static function find($slug): array
-    {
-        // return Arr::first(static::all(), function ($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
+     // jika nama tabel beda pakai cara ini
+    // protected $table = "blog_posts"; // versi jamaknya
 
-
-        // :: digunakan untuk mengakses static
-        // keyword static digunakan untuk merujuk ke class ini sendiri / this
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    // secara default id, jika bukan id pakai cara ini
+    // protected $primaryKey = "id";
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
